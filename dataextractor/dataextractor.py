f2cc583116c1438e74935b7e1ce0d1407ac712c5
Water-Melon-Ice <adrian.dev@outlook.de>
2023-05-08 16:26:24 +0200

    Finished Data Extractor from HTML

---
+++ dataextractor/dataextractor.py
@@ -0,0 +1,46 @@
+import argparse
+from bs4 import BeautifulSoup
+
+
+def extract(source, destination, tags, attrs):
+    file = open(source, encoding="utf-8")
+    lines = file.read()
+    soup = BeautifulSoup(lines, 'html.parser')
+    for tag in tags:
+        for element in soup.find_all(tag, attrs):
+            if(element.string == None):
+                continue
+            print(element.string)
+            destination.write(element.get_text(strip=True) + "\n")
+
+
+if (__name__ == "__main__"):
+    argParser = argparse.ArgumentParser()
+    
+    argParser.add_argument('outputfile')
+    # argParser.add_argument('-t', '--threads', type=int, default='1')
+    argParser.add_argument('-e', '--elements', nargs='+', required=True)
+    argParser.add_argument('-a', '--attributes', nargs='*')
+    group = argParser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-i', '--inputs', nargs='+')     #list of paths
+    group.add_argument('-f', '--inputfile', type=open)  #file with a list of paths
+
+    args = argParser.parse_args()
+    output = open(args.outputfile, mode='a+')
+    tags = args.elements
+
+    attrs = {}
+    if(args.attributes != None):
+        for attr in args.attributes:
+            key,val = attr.split("=")
+            attrs[key] = val
+
+    f = args.inputfile
+    if(f != None):
+        for line in f:
+            line.rstrip()
+            extract(line, output, tags, attrs)
+    
+    inputs = args.inputs
+    for i in inputs:
+        extract(i, output, tags, attrs)
